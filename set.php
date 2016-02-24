1fd3ded5cc606c7af680205f3747de7e8ed3ad23
ircphp <ircphp@gmail.com>
2016-02-24 13:57:36 +0200

    2

--- set.php
+++ set.php
@@ -1,18 +1,19 @@
 <?php
 
-if (date('Y') < 2016) {
-  $nmea_src = '/dev/ttyS2';
+define('NMEA_DEV', '/dev/ttyS2'); // GPS device with NMEA messages
+define('NMEA_LATENCY', 2);        // Empirical GPS latency in seconds
 
-  if ($gps_src = fopen($nmea_src, 'r'))
-    while (!feof($gps_src) && false !== ($gps_res = fgets($gps_src)))
-      if (preg_match('/^\$GPRMC\,(\d{6}).*\,(\d{6})\,/', $gps_res, $gps_msg)) {
-        fclose($gps_src);
+//date_default_timezone_set('Europe/Kiev'); // Timezone, may already set in env
 
-        $date = DateTime::createFromFormat('Hisdmy', $gps_msg[1].$gps_msg[2]);
-        $date->add(new DateInterval('PT2H2S'));
-        $dttm = $date->format('j M Y H:i:s');
+if (date('Y') < 2016 && $gps_src = fopen(NMEA_DEV, 'r'))
+  while (!feof($gps_src) && false !== ($nmea_line = fgets($gps_src)))
+    if (preg_match('/^\$GPRMC\,(\d{6}).*\,(\d{6})\,/', $nmea_line, $mline)) {
+      fclose($gps_src);
 
-        `date -s '$dttm'`;
-        exit();
-      }
-}
+      $oDate = DateTime::createFromFormat('Hisdmy', $mline[1].$mline[2]);
+      $oDate->add(new DateInterval('PT'.(date('Z') + NMEA_LATENCY).'S'));
+      $dttm = $oDate->format('j M Y H:i:s');
+
+     `date -s '$dttm'`;
+      exit();
+  }
